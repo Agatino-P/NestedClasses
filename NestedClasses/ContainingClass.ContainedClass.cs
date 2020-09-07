b8910d1b2ddb5f16506312640aa6404901cbb0c8
agatino.work@gmail.com <agatino.work@gmail.com>
2020-09-07 10:13:24 +0200

    Add project files.

---
+++ NestedClasses/ContainingClass.ContainedClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NestedClasses
+{
+    public  partial class ContainingClass
+    {
+        partial class ContainedClass
+        {
+            public int Numero;
+            public ContainedClass(int i)
+            { Numero = i*10; }
+        }
+    }
+}
+
+    
---
+++ NestedClasses/ContainingClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NestedClasses
+{
+    public partial class ContainingClass
+    {
+        private ContainedClass containedClass;
+        public ContainingClass( int i)
+        {
+        containedClass = new ContainedClass(i);
+        }
+
+        public int GetValue() => containedClass.Numero;
+    }
+}
